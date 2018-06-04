111bc5d5cab4c516a7eecb75dd637722f1b1aa37
RJ722 <rahul722j@gmail.com>
2018-06-04 20:16:07 +0530

    Remove nargs=1

--- generators/main.py
+++ generators/main.py
@@ -18,9 +18,8 @@ def _parse_args():
     version = "vulture-whitelist {0}".format(__version__)
     parser = argparse.ArgumentParser(prog='vulture-whitelist')
     parser.add_argument('--version', action='version', version=version)
-    parser.add_argument(
-        'framework', nargs=1, choices=CREATORS.keys(), help=(
-            'Framweork to generate a whitelist for.'))
+    parser.add_argument('framework', choices=CREATORS.keys(), help=(
+        'Framweork to generate a whitelist for.'))
     parser.add_argument('--sip', nargs=1, default=['sip'], help=(
         'The sip binary to use when creating a whitelist for PyQt.'))
     return parser.parse_args()
