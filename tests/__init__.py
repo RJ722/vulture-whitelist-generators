e58e15ae7f22c466dd0488cee8d1e9e4a2250d7f
RJ722 <rahul722j@gmail.com>
2018-06-08 04:42:54 +0530

    tests/__init__.py: TESTS and DIR are same

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,5 @@
 import os.path
 
-DIR = os.path.dirname(os.path.abspath(__file__))
-REPO = os.path.dirname(DIR)
-TESTS = os.path.join(REPO, 'tests')
-SAMPLE_WHITELISTS = os.path.join('tests', 'test-data', 'sample-whitelists')
+TESTS = os.path.dirname(os.path.abspath(__file__))
+SAMPLE_WHITELISTS = os.path.join(TESTS, 'test-data', 'sample-whitelists')
+
